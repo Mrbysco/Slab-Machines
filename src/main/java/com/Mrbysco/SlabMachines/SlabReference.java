8ea68ad37d5853c6f82255e16ae708a7bc538a79
Mrbysco <Heeybysco@live.nl>
2018-10-01 21:26:44 +0200

    Fix load order

--- src/main/java/com/Mrbysco/SlabMachines/SlabReference.java
+++ src/main/java/com/Mrbysco/SlabMachines/SlabReference.java
@@ -6,7 +6,7 @@ public class SlabReference {
 	public static final String MOD_PREFIX = "slabmachines:";
 	public static final String VERSION = "1.0.1";
 	public static final String ACCEPTED_VERSIONS = "[1.12]";
-	public static final String DEPENDENCIES = "after:fastbench;after:fastfurnace;required-before:mcmultipart;";
+	public static final String DEPENDENCIES = "after:fastbench;after:fastfurnace;after:tconstruct;required-before:mcmultipart;";
 			
 	public static final String CLIENT_PROXY_CLASS = "com.Mrbysco.SlabMachines.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "com.Mrbysco.SlabMachines.proxy.ServerProxy";
