f042b522a39e1efb27d849aa32dfcde7eaf4557c
Mrbysco <Heeybysco@live.nl>
2018-10-01 20:51:13 +0200

    Added some recipes
    to stop conflicts from making stuff uncraftable

--- src/main/java/com/Mrbysco/SlabMachines/SlabReference.java
+++ src/main/java/com/Mrbysco/SlabMachines/SlabReference.java
@@ -4,7 +4,7 @@ public class SlabReference {
 	public static final String MOD_ID = "slabmachines";
 	public static final String MOD_NAME = "Slab Machines";
 	public static final String MOD_PREFIX = "slabmachines:";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.0.1";
 	public static final String ACCEPTED_VERSIONS = "[1.12]";
 	public static final String DEPENDENCIES = "after:fastbench;after:fastfurnace;required-before:mcmultipart;";
 			
